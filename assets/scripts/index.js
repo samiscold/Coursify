80ad57c1f7aada57013d7e2f7ee8f60259d5ad5a
Edon Abdullahu <edonabdullahu2@gmail.com>
2022-06-30 00:18:23 -0400

    Log in Form done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@ function buttonContinue() {
   let x = document.getElementById("intro-section");
   x.style.transform = 'translate(0,-100%)'
   x.style.transition = "1s"
-  setTimeout(function () { window.location = "welcome.html"; }, 1100);
+  setTimeout(function () { window.location = "login.html"; }, 1100);
 }
 
 
