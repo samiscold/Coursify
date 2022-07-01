498364117640fe24070d24d533667cf94d610d9c
samiscold <62956581+samiscold@users.noreply.github.com>
2022-07-01 17:59:08 +0200

    music-icon-animation-fixed-onclick

--- assets/scripts/howler.js
+++ assets/scripts/howler.js
@@ -3,35 +3,43 @@
 var sound = new Howl({
     src: ['assets/audios/music.mp3'],
     preload: true,
-    volume: 0.05,
+    volume: 0.1,
     autoplay: false,
     onplay:function() {
-      console.log("test");
+      console.log("works");
     }
 
   });
 
 
   sound.on('load', function() {
-
+    document.getElementById("wave").style.display ="inline-block";
     //  var startButton = document.getElementById('start');
-     var svganim = document.getElementById("wave");
+    var svganim = document.getElementById("wave");
 
     svganim.addEventListener('click', function() {
     if(sound.playing()) {
       sound.pause();
-      
+      this.classList.remove("run");
 
-
-    } else {
-      sound.play();
       this.classList.toggle('stop');
+      
+    } else {
+
+      sound.play();
+      this.classList.remove("stop");
+
+      this.classList.toggle('run');
+
+      
     }
 
   })
   })
- 
 
+  // const mIcon = document.querrySelectorAll('.music-icon ')
+  // mIcon.forEach
+ 
   
     // svganim.addEventListener('click', function() {
     // svganim.classList.toggle('stop')
