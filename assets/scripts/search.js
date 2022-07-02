42d54e8c89ffa323be8c4ac0edfe63fa7b199470
Edon Abdullahu <edonabdullahu2@gmail.com>
2022-07-01 23:25:26 -0400

    filter with search bar done

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 const people = [
     { name: 'index.html' },
@@ -55,6 +56,40 @@ function clearList() {
     // looping through each child of the search results list and remove each child
     while (list.firstChild) {
         list.removeChild(list.firstChild)
+=======
+
+
+let cards = document.querySelectorAll('.course-box')
+
+
+//A little delay
+let typingTimer;
+let typeInterval = 500;
+let searchInput = document.getElementById('search');
+
+searchInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(liveSearch, typeInterval);
+});
+
+function liveSearch() {
+    // Locate the card elements
+    let cards = document.querySelectorAll('.course-box')
+    // Locate the search input
+    let search_query = document.getElementById("search").value;
+    // Loop through the cards
+    for (var i = 0; i < cards.length; i++) {
+        // If the text is within the card...
+        if (cards[i].innerText.toLowerCase()
+            // ...and the text matches the search query...
+            .includes(search_query.toLowerCase())) {
+            // ...remove the `.is-hidden` class.
+            cards[i].classList.remove("is-hidden");
+        } else {
+            // Otherwise, add the class.
+            cards[i].classList.add("is-hidden");
+        }
+>>>>>>> Stashed changes
     }
 }
 
